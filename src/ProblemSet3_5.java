b57d43c0fb4a37fbffe24c2e94fc0f054ab2e818
tegenton <tegenton@ucvts.tec.nj.us>
2018-10-23 20:03:55 +0000

    Finish palindrom, functions still need to be tested

--- src/ProblemSet3_5.java
+++ src/ProblemSet3_5.java
@@ -1,16 +1,16 @@
 /**
  * INSTRUCTIONS.
- * 
+ *
  * The following exercises are, perhaps, more mathematically inclined than previous
  * problem sets. While they might be challenging, they are certainy doable.
- * 
+ *
  * You can (and should!) solve each of them using only the operators, conditional control
  * structures, and iterative control strucurs we've covered. You are not permitted to use
  * recursive code in your solutions.
- * 
+ *
  * Please be mindful of the expected outputs, as your code will be tested against that
  * directly. If you're unsure, please ask for clarification.
- * 
+ *
  * This problem set is worth 25 points and is due no later than 11:59pm on October 28, 2018.
  */
 
@@ -22,23 +22,23 @@ public class ProblemSet3_5 {
         // test your solutions here
 
         ps.primes(1,1000);
-        
+
         ps.leapYears(10);
-        
-        ps.palindromicNumbers(202);
-        
+
+        ps.palindromicNumbers(1001);
+
         ps.fibonacci(5);
-        
+
         ps.multiples(2, 3, 20);
     }
 
     /**
      * How many prime numbers are there between @start and @end, where @start and @end
      * are positive integers in the range [1, @Integer.MAX_VALUE]?
-     * 
+     *
      * Print your solution in the following formats: "There is X prime number."
      *                                               "There are X prime numbers."
-     * 
+     *
      * @param start
      * @param end
      */
@@ -47,11 +47,11 @@ public class ProblemSet3_5 {
         int count = 0;
         for (; start < end; start++) {
             if (isPrime(start))
-            	count++;
+                count++;
         }
         System.out.println("There " + ((count != 1) ? "are " + count + " prime numbers." : "is " + count + " prime number."));
     }
-    
+
     private boolean isPrime(int num) {
         if (num < 2) return false;
         if (num == 2) return true;
@@ -63,16 +63,15 @@ public class ProblemSet3_5 {
 
     /**
      * What are the next @count leap years?
-     * 
+     *
      * Print your solution in the following formats: "The next leap year is X."
      *                                               "The next 2 leap years are X and Y."
      *                                               "THe next N leap years are A, ..., X, Y, and Z."
-     * 
+     *
      * @param count
      */
 
     public void leapYears(int count) {
-    	// TODO: dont use arrays
         System.out.print("The next " + ((count > 1) ? count + " leap years are " : " leap year is "));
         if (count == 1)
             System.out.print("2020");
@@ -90,87 +89,87 @@ public class ProblemSet3_5 {
 
     /**
      * Is @number a palindromic number?
-     * 
+     *
      * Print your solution in the following formats: "X is a palindromic number."
      *                                               "X is not a palindromic number."
-     *                                               
+     *
      * @param number
      */
 
     public void palindromicNumbers(int number) {
         boolean palindromic = true;
-        // TODO: use % and / to do with an int
-        String palindrome = number + "";
-        for (int i = 0; i < palindrome.length() / 2; i++) {
-            if (palindrome.charAt(i) == palindrome.charAt(palindrome.length() -  (i + 1)))
-                continue;
-            else 
-                palindromic = false;
+        int length = (int)(Math.log10(number)+1);
+        System.out.println(length);
+        int i = 0;
+        while (i < length && palindromic) {
+            if ( (int) (number / Math.pow(10,length - (i + 1)) % 10) == (int) (number / Math.pow(10,i)) % 10);
+            else palindromic = false;
+            i++;
         }
         System.out.println(number + " is" + ((palindromic) ? "" : " not") + " a palindromic number");
     }
 
     /**
      * What is the @nth Fibonacci number, where @n is a positive integer?
-     * 
+     *
      * Print your solution in the following formats: "The 21st Fibonacci number is X."
      *                                               "The 22nd Fibonacci number is X."
      *                                               "The 23rd Fibonacci number is X."
      *                                               "The 24th Fibonacci number is X."
-     *                                               
+     *
      * @param n
      */
 
     public void fibonacci(int n) {
-    	
-    	int previous = 1;
-    	int current = 1;
-    	int next;
-    	int count = n - 2;
-    	
-    	while (count > 0) {
-    		next = previous + current;
-    		previous = current;
-    		current = next;
-    		count--;
-    	}
-    	
+
+        int previous = 1;
+        int current = 1;
+        int next;
+        int count = n - 2;
+
+        while (count > 0) {
+            next = previous + current;
+            previous = current;
+            current = next;
+            count--;
+        }
+
         String indicator;
         switch ((n / 10 == 1) ? n : n % 10) {
-        	case 1:
-        		indicator = "st";
-        		break;
-        	case 2:
-        		indicator = "nd";
-        		break;
-        	case 3:
-        		indicator = "rd";
-        		break;
-        	default:
-        		indicator = "th";
+            case 1:
+                indicator = "st";
+                break;
+            case 2:
+                indicator = "nd";
+                break;
+            case 3:
+                indicator = "rd";
+                break;
+            default:
+                indicator = "th";
         }
-        System.out.println("The " + n + indicator + " number is " + current + "."); 
+        System.out.println("The " + n + indicator + " number is " + current + ".");
     }
 
     /**
      * What is the sum of all multiples of @x and @y less than @limit, where @x, @y, and
      * @limit are positive integers?
-     * 
+     *
      * Print your solution in the following format: "The sum is X."
-     * 
+     *
      * @param limit
      */
 
     public void multiples(int x, int y, int limit) {
-	    int sum = 0;
-	    for (int i = 0; i < limit / x; i++) {
-	    	sum += x * i;
-	    }
-	    for (int i = 0; i < limit / y; i++) {
-	    	if (i % x != 0) {
-	    		sum += y * i;
-	    	}
-	    }
-	    System.out.println("The sum is " + sum + ".");
+        int sum = 0;
+        for (int i = 0; i < limit / x; i++) {
+            sum += x * i;
+        }
+        for (int i = 0; i < limit / y; i++) {
+            if (i % x != 0) {
+                sum += y * i;
+            }
+        }
+        System.out.println("The sum is " + sum + ".");
     }
 }
